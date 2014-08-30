da60c555e9085df872b18aced12085570a06461d
Anh Tuan <atn832@gmail.com>
2014-08-30 16:25:24 -0700

    maprenderer

---
+++ public_html/js/components/line/renderer/maprenderer.jsx
@@ -0,0 +1,42 @@
+/** @jsx React.DOM */
+function initializeMaps(lat, lng, element) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+        zoom: 11,
+        center: latlng,
+        mapTypeId: 'roadmap'
+    }
+    map = new google.maps.Map(element, mapOptions);
+
+    marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
+}
+
+var MapRenderer = React.createClass({
+    render: function() {
+        var divStyle = {};
+        if (this.isLocationValid()) {
+            divStyle = {
+                width:"50%",
+                height: "300px"
+            };
+        }
+        return (
+            <div ref="map" style={divStyle}>
+            </div>
+        );
+    },
+    componentDidMount: function() {
+        var source = this.props.source;
+        if (this.isLocationValid()) {
+            var loc = source.location;
+            initializeMaps(loc.latitude, loc.longitude, this.refs.map.getDOMNode());
+        }
+    },
+    isLocationValid: function() {
+        var source = this.props.source;
+        return source.location && source.location.latitude && source.location.longitude;
+    }
+});
