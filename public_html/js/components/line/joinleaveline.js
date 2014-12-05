6a7141be825d9370fc56f62db0979e141aa69e2c
Anh Tuan Nguyen <atn832@gmail.com>
2014-12-05 15:27:33 -0800

    fixed join/leave event rendering

--- public_html/js/components/line/joinleaveline.js
+++ public_html/js/components/line/joinleaveline.js
@@ -1,16 +1,17 @@
 /** @jsx React.DOM */
 import DateRenderer from "./renderer/daterenderer";
 import DeviceRenderer from "./renderer/devicerenderer";
+import EventTypes from "../../data/eventtype";
 
 var JoinLeaveLine = React.createClass({
     render: function() {
         var event = this.props.event;
         var typeID = event.eventType.ID;
-        var event = typeID === EventTypes.Join? "Join": "Leave";
+        var eventName = typeID === EventTypes.Join? "Join": "Leave";
         return (
             <div>
                 <span className="line clickable">
-                    {DateRenderer.render(event.source)} {DeviceRenderer.render(event.source)} {event.source.entity.name}&gt; {event}
+                    {DateRenderer.render(event.source)} {DeviceRenderer.render(event.source)} {event.source.entity.name}&gt; {eventName}
                 </span>
             </div>
         );
