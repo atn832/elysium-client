b6ef78e5442fdd1881b530d2e1b8a052996cc7c0
Anh Tuan Nguyen <atn832@gmail.com>
2014-12-05 19:36:18 -0800

    moved jquery to build pack

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
+var compression = require('compression');
 
-app.use(express.static(__dirname + '/public_html'));
+//var oneDay = 86400000;
+
+app.use(compression());
+app.use(express.static(__dirname + '/build'));
+//app.use(express.static(__dirname + '/build', { maxAge: oneDay }));
 
 app.listen(process.env.PORT || 3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var path=require("path");
-
+var webpack=require("webpack");
 module.exports = {
     entry: "./built_jsx/js/app.js",
     output: {
@@ -8,7 +8,13 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'es6-loader' }
+            { test: /\.js$/, loader: 'es6-loader' },
+            { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            "$": path.join(__dirname, "public_html/js/lib/jquery-2.1.1.min")
+        })
+    ]
 };
