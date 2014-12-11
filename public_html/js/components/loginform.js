24fc2ddd212997ff14bd331ed6a1af6ee1bfa439
Anh Tuan Nguyen <atn832@gmail.com>
2014-12-10 17:31:04 -0800

    improved login form

--- public_html/js/components/loginform.js
+++ public_html/js/components/loginform.js
@@ -38,13 +38,16 @@ var LoginForm = React.createClass({
     render: function() {
         return (
             <div className="d-f fd-c h-100 w-100 pos-r jc-c">
-                <form className="login d-f fd-c jc-c" onSubmit={this.handleSubmit}>
+                <form className="d-f fd-c jc-c" onSubmit={this.handleSubmit}>
                     <h1 className="as-c">Elysium</h1>
-                    <input type="text" className="d-b as-c w-75 lh-2 px-8 mb-12 bz-bb" placeholder="Channel" ref="channel" value={this.state.channel} onChange={this.handleChange}/>
-                    <input type="hidden" className="d-b as-c w-75 lh-2 px-8 mb-12 bz-bb" placeholder="Password" ref="password" value={this.state.password} onChange={this.handleChange}/>
-                    <input type="text" className="d-b as-c w-75 lh-2 px-8 mb-12 bz-bb" placeholder="Login" ref="login" value={this.state.login} onChange={this.handleChange} autoFocus/>
-                    <input type="submit" className="button as-c w-75 mb-12" value="Sign in" onClick={this.onSubmit}/>
-                    <a href={"?chanName=" + this.state.channel + "&nick=" + this.state.login} className="d-b as-c link">bookmark this link for direct login</a>
+                    <input type="text" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Channel" ref="channel" value={this.state.channel} onChange={this.handleChange}/>
+                    <input type="hidden" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Password" ref="password" value={this.state.password} onChange={this.handleChange}/>
+                    <input type="text" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Login" ref="login" value={this.state.login} onChange={this.handleChange} autoFocus/>
+                    <button type="submit" className="button as-c w-75 mw-400 mb-12 tr" onClick={this.onSubmit} disabled={this.props.isSigningIn}>
+                        {this.props.isSigningIn? "Signing in": "Sign in"}
+                        {this.props.isSigningIn? <i className="ml-4 fa fa-spinner fa-spin"></i>: null}
+                    </button>
+                    <a href={"?chanName=" + this.state.channel + "&nick=" + this.state.login} className="d-b as-c link">Bookmark this link for direct login</a>
                 </form>
                 {this.props.status? <div className="mt-30">{this.props.status}</div>: null}
                 {this.props.error? <iframe className="w-100" srcDoc={this.props.error} />: null }
