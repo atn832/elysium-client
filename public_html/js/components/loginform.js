2df7f40827416e4c2e1d5ed8a3eca9a7ad343fda
Anh Tuan <atn832@gmail.com>
2014-12-26 09:51:48 -0600

    one click login by default

--- public_html/js/components/loginform.js
+++ public_html/js/components/loginform.js
@@ -25,6 +25,12 @@ var LoginForm = React.createClass({
 
         this.props.onLogin(channel, password, login);
     },
+    onKeyUp: function(e) {
+        if (e.keyCode === 13) {
+            var href = this.refs.signin.props.href;
+            window.location.href = href;
+        }
+    },
     render: function() {
         var linkParams = [];
         if (this.state.channel)
@@ -36,14 +42,13 @@ var LoginForm = React.createClass({
                 <form className="d-f fd-c jc-c" onSubmit={this.handleSubmit}>
                     <h1 className="as-c">Elysium</h1>
                     {this.props.status? <div className="as-c w-75 mw-400 mb-12">{this.props.status}</div>: null}
-                    <input type="text" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Channel" ref="channel" value={this.state.channel} onChange={this.handleChange}/>
-                    <input type="hidden" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Password" ref="password" value={this.state.password} onChange={this.handleChange}/>
-                    <input type="text" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Username" ref="login" value={this.state.login} onChange={this.handleChange} autoFocus/>
-                    <button type="submit" className="button as-c w-75 mw-400 lh-2 mb-12 tr" onClick={this.onSubmit} disabled={this.props.isSigningIn}>
+                    <input type="text" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Channel" ref="channel" value={this.state.channel} onChange={this.handleChange} onKeyUp={this.onKeyUp}/>
+                    <input type="hidden" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Password" ref="password" value={this.state.password} onChange={this.handleChange} onKeyUp={this.onKeyUp}/>
+                    <input type="text" className="d-b as-c w-75 mw-400 lh-2 px-8 mb-12 bz-bb" placeholder="Username" ref="login" value={this.state.login} onChange={this.handleChange} autoFocus onKeyUp={this.onKeyUp}/>
+                    <a href={"?" + linkParams.join("&")} className="button fz-m as-c w-75 mw-400 lh-2 tr td-n ta-c bz-bb" disabled={this.props.isSigningIn} ref="signin">
                         {this.props.isSigningIn? "Signing in": "Sign in"}
                         {this.props.isSigningIn? <i className="ml-8 fa fa-spinner fa-spin"></i>: null}
-                    </button>
-                    <a href={"?" + linkParams.join("&")} className="d-b as-c link">Bookmark this link for direct login</a>
+                    </a>
                 </form>
                 {this.props.debug && this.props.error? <iframe className="w-100" srcDoc={this.props.error} />: null }
             </div>
