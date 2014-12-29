5b188964f1fe7df8a1e0875f9a3c08d24fd33a0d
Anh Tuan <atn832@gmail.com>
2014-12-29 09:33:13 -0600

    more tag cleanup

--- public_html/js/components/line/renderer/tag.js
+++ public_html/js/components/line/renderer/tag.js
@@ -6,10 +6,8 @@ var Tag = React.createClass({
     render: function() {
         var source = this.props.source;
         return (
-            <span className={"d-ib square c-w bdr-3 bgc-" + (source.entity.ID % 5)}>
-                <span className="square d-f fd-c jc-c ta-c">
-                    <span className="fz-l">{source.entity.name.substring(0, 1).toUpperCase()}</span>
-                </span>
+            <span className={"d-ib square c-w ta-c bdr-3 bgc-" + (source.entity.ID % 5)}>
+                <span className="d-ib va-c pos-r fz-l">{source.entity.name.substring(0, 1).toUpperCase()}</span>
             </span>
         );
     }
